c13638c3e7fbad5d065684a16342da2a762f716d
jadenjohhnson <122333343+jadenjohhnson@users.noreply.github.com>
2024-09-14 04:43:06 +0000

    Completed tasks 3,4, and 5

--- app/main.c
+++ app/main.c
@@ -2,13 +2,21 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <unistd.h>
+#include "../src/lab.h"
+#include <readline/readline.h>
+#include <readline/history.h>
 
 int main(int argc, char **argv)
 {
-  printf("hello world\n");
+  // printf("hello world\n");
+
+  //set prompt for when shell is started
+  setenv("MY_PROMPT", "(.)(.)", 1);
+  char *prompt = getenv("MY_PROMPT");
   
   int c;
-  while((c = getopt (argc, argv, "abcv" )) != -1)
+  char *line;
+  while((c = getopt (argc, argv, "abcvt" )) != -1)
     switch (c)
     {
       case 'a':
@@ -21,11 +29,27 @@ int main(int argc, char **argv)
         printf("get c here\n");
         break;
       case 'v':
-        printf("This is the version of the project!!\n");
+      //This is step 1: print the lab version number
+        printf("Lab Version Major is: %d\n", lab_VERSION_MAJOR);
+        printf("Lab Version minor is: %d\n", lab_VERSION_MINOR);
+        return -1;
         break;
+      case 't':
+        //I will be testing readline here, just tryin to print what I type
+        using_history();
+        while ((line=readline(prompt))){
+          if (strcmp(line, "exit") == 0) {
+              free(line);
+              break;
+          }
+          printf("%s\n", line);
+          add_history(line);
+          free(line);
+          }
+          break;
       case '?':
-      //   if (isprint(optopt))
-      //     fprintf(stderr, "Unkown option", optopt);
+        // if (isprint(optopt))
+          fprintf(stderr, "Unkown option");
       //     else  fprintf(stderr, "Unknown option", optopt);
         return 1;
       default:
