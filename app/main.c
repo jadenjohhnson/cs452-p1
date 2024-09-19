d41a26798caedd5dcebfb8ffc1d555cabd3e4f0c
jadenjohhnson <122333343+jadenjohhnson@users.noreply.github.com>
2024-09-19 00:21:13 +0000

    code now working without 't' flag

--- app/main.c
+++ app/main.c
@@ -16,45 +16,29 @@ int main(int argc, char **argv)
 
   int c;
   char *line;
-  while((c = getopt (argc, argv, "abcvt" )) != -1)
+  while((c = getopt (argc, argv, "v" )) != -1)
     switch (c)
     {
-      case 'a':
-        printf("get a here\n");
-        break;
-      case 'b':
-        printf("get b here\n");
-        break;
-      case 'c':
-        printf("get c here\n");
-        break;
       case 'v':
       //This is step 1: print the lab version number
-        printf("Lab Version Major is: %d\n", lab_VERSION_MAJOR);
-        printf("Lab Version minor is: %d\n", lab_VERSION_MINOR);
+        printf("Lab Version is: %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+        free(prompt);
         return -1;
         break;
-      case 't':
-        using_history();
-        while ((line=readline(prompt))){
-          if (strcmp(line, "exit") == 0) {
-              free(line);
-              break;
-          }
-          printf("%s\n", line);
-          add_history(line);
-          free(line);
-          }
-          break;
-      case '?':
-        // if (isprint(optopt))
-          fprintf(stderr, "Unkown option");
-      //     else  fprintf(stderr, "Unknown option", optopt);
-        return 1;
       default:
-        printf("reached default");
+        break;
     }
 
+  using_history();
+  while ((line = readline(prompt)) != NULL){
+    if (strcmp(line, "exit") == 0) {
+        free(line);
+        break;
+    }
+    printf("%s\n", line);
+    add_history(line);
+    free(line);
+  }
   free(prompt);
   return 0;
 }
